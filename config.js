89ecd2e32411ab380c0b805fac0234dc89033f39
Troza <trozah@gmail.com>
2020-11-06 17:39:11 -0600

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '1233569173666917',
+        clientSecret: 'b2671afbebf92fa4e500ab3816bd48b9'
+    }
 }
--- routes/users.js
+++ routes/users.js
@@ -51,7 +51,6 @@ router.post('/signup', cors.corsWithOptions, (req, res) => {
     );
 });
 
-
 router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
     const token = authenticate.getToken({_id: req.user._id});
     res.statusCode = 200;
@@ -68,6 +67,15 @@ router.get('/logout', cors.corsWithOptions, (req, res, next) => {
         const err = new Error('You are not logged in!');
         err.status = 401;
         return next(err);
+    }
+});
+
+router.get('/facebook/token', passport.authenticate('facebook-token'), (req, res) => {
+    if (req.user) {
+        const token = authenticate.getToken({_id: req.user._id});
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({success: true, token: token, status: 'You are successfully logged in!'});
     }
 });
 
